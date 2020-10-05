4ba3b0eaac0c4c69df1c69f850ed93d51caf9e93
Basamawi <72248988+Basamawi@users.noreply.github.com>
2020-10-05 20:50:27 +0300

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   encoded_jwt = jwt.encode({'some': 'payload'}, 'secret', algorithm='HS256')
-    return "Mwahaha, application.py!" + encoded_jwt
+    return encoded_jwt
     
 if __name__ == "__main__":
     app.run(debug=True)
